c028455ae6806be378e62277e5a22afe5d8f6e7c
Gunjan Dhanuka <lucifergd192@gmail.com>
2021-03-06 12:14:07 +0530

    Test commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-
     return MyAppState();
   }
 }
@@ -16,20 +15,15 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Portfolio Project",
+      title: "Portfolio App",
       home: Scaffold(
         backgroundColor: Colors.grey[200],
         appBar: AppBar(
           title: Text("Welcome"),
           backgroundColor: Colors.blueGrey,
         ),
-        body: SafeArea(
-            child: LoginScreen()
-        ),
-
+        body: SafeArea(child: LoginScreen()),
       ),
     );
   }
-
 }
-
