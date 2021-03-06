68de794fe37f65f246ee617fdfb704858ca27658
Siddharth Khincha <siddharth.khincha@yahoo.in>
2021-03-06 15:10:36 +0530

    Signup updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import './LoginScreen.dart';
 
 void main() => runApp(MyApp());
